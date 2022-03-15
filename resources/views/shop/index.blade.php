1ffdc5d2bf25372fab2fa8d058a19cdfb9adb57b
Adrian Ocana <adrian.ocana01@gmail.com>
2022-03-15 20:41:53 +0000

    shop filter

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -13,7 +13,7 @@
             <div class="border-t border-gray-200 px-4 py-6">
                 <h3 class="-mx-2 -my-3 flow-root">
                     <!-- Expand/collapse section button -->
-                    <button type="button" id="brandButton" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-1" aria-expanded="false">
+                    <button type="button" id="brandButton" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
                         <span class="font-medium text-gray-900"> Category </span>
                         <span class="ml-6 flex items-center">
                             <svg id="filter-section-1-plus" class=" h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -25,54 +25,21 @@
                         </span>
                     </button>
                 </h3>
-                <!-- Filter section, show/hide based on section state. -->
+                <!-- Expand/collapse -->
                 <div class="pt-6" id="filter-section-mobile-1" style="display:none;">
                     <div class="space-y-6">
                         <div class="flex items-center">
-                            <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
+                            <input id="filter-mobile-category-0" name="brand" value="Apple" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
                             <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500">Apple</label>
                         </div>
                         <div class="flex items-center">
-                            <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
+                            <input id="filter-mobile-category-0" name="brand" value="HP" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
                             <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500">HP</label>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="border-t border-gray-200 px-4 py-6">
-                <h3 class="-mx-2 -my-3 flow-root">
-                    <!-- Expand/collapse section button -->
-                    <button type="button" id="brandButton" class="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500" aria-controls="filter-section-1" aria-expanded="false">
-                        <span class="font-medium text-gray-900"> Category </span>
-                        <span class="ml-6 flex items-center">
-                            <svg id="filter-section-1-plus" class=" h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
-                            </svg>
-                            <svg id="filter-section-1-cross" class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" style="display: none;">
-                                <path fill-rule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </button>
-                </h3>
-                <!-- Filter section, show/hide based on section state. -->
-                <div class="pt-6" id="filter-section-mobile-1" style="display:none;">
-                    <div class="space-y-6">
-                        <div class="flex items-center">
-                            <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
-                            <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500">Apple</label>
-                        </div>
-                        <div class="flex items-center">
-                            <input id="filter-mobile-category-0" name="category[]" value="new-arrivals" type="checkbox" class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500">
-                            <label for="filter-mobile-category-0" class="ml-3 min-w-0 flex-1 text-gray-500">HP</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- <input type="checkbox" name="brand" value="Apple">Apple</input>
-            <input type="checkbox" name="brand" value="Logitech">Logitech</input>
-            <input type="checkbox" name="brand" value="HP">HP</input> -->
             <input type="submit" value="Filter" class="bg-blue-500">
         </form>
         @forelse($products as $product)
