947efe22cd907ccb6bc8b36d0a2c57406366876a
Adrian Ocana <adrian.ocana01@gmail.com>
2022-04-20 19:43:18 +0200

    create: Orders Model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, Billable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -41,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function orders()
+    {
+        $this->hasMany(Order::class);
+    }
 }
