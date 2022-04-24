8b6d7f9c551577a23218054afe897d9a9d7f0181
Adrian Ocana <adrian.ocana01@gmail.com>
2022-04-24 13:33:50 +0200

    Mobile links

--- src/resources/views/admin/orders/show.blade.php
+++ src/resources/views/admin/orders/show.blade.php
@@ -1,9 +1,21 @@
 <x-admin-layout>
     <p class="text-2xl">Order #{{ $order->id }}</p>
-    <div>
-        <p>Customer</p>
-        <p>Name:{{ $order->user->name }}</p>
-        <p>Email:{{ $order->user->email }}</p>
+    <div class="flex">
+        <div>
+            <p>Customer</p>
+            <p>Name:{{ $order->user->name }}</p>
+            <p>Email:{{ $order->user->email }}</p>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+            <p>Order status</p>
+            <p>{{ $order->status }}</p>
+        </div>
+
     </div>
     <div>
 
@@ -11,7 +23,7 @@
             <thead class="bg-gray-50">
                 <tr>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Name
+                        Product
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Quantity
